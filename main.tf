e415615d6d79f1aedb537f74eaabce4d60dce42a
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-04-19 15:47:07 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,67 @@
-
 terraform {
+ 
   required_version = ">= 0.12.26"
+}
+
+module "gcp-network" {
+  source       = "terraform-google-modules/network/google"
+  version      = "~> 2.5"
+  project_id   = var.project
+  network_name = "kubernetes"
+  subnets = [
+    {
+      subnet_name   = "subnet"
+      subnet_ip     = "10.10.0.0/16"
+      subnet_region = var.region
+    },
+   ]
+  secondary_ranges = {
+    "subnet" = [
+      {
+        range_name    = "pods"
+        ip_cidr_range = "10.20.0.0/16"
+      },
+      {
+        range_name    = "services"
+        ip_cidr_range = "10.30.0.0/16"
+      },
+    ]
+  }
+}
+
+  
+module "gke" {
+  source                 = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  project_id             = var.project
+  name                   = "gke"
+  regional               = true
+  region                 = var.region
+  network                = module.gcp-network.network_name
+  subnetwork             = module.gcp-network.subnets_names[0]
+  ip_range_pods          = "pods"
+  ip_range_services      = "services"
+  node_pools = [
+    {
+      name                      = "node-pool"
+      machine_type              = "e2-medium"
+      node_locations            = "europe-west3-a,europe-west3-b"
+      min_count                 = 1
+      max_count                 = 2
+      disk_size_gb              = 30
+    },
+  ]
+}
+
+    
+module "gke_auth" {
+  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  depends_on   = [module.gke]
+  project_id   = var.project
+  location     = module.gke.location
+  cluster_name = module.gke.name
+}
+
+resource "local_file" "kubeconfig" {
+  content  = module.gke_auth.kubeconfig_raw
+  filename = "kubeconfig-env_name"
 }
