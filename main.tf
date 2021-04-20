076e0797263dfb774bd5e2d355769d38e5cb7025
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-04-20 14:46:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,19 @@
 terraform {
- 
-  required_version = ">= 0.12.26"
+ required_providers {
+  kubernetes = {
+   source = "hashicorp/kubernetes"
+   version = ">= 2.0.2"
+   }
+  helm = {
+   source = "hashicorp/helm"
+   version = ">= 2.0.2"
+   }
+  }
+ required_version = ">= 0.12.26"
+}
+
+terraform {
+
 }
 
 module "gcp-network" {
