aa6fb5bce3d9788c4d97ef9abc4be177d69e1752
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-04-28 16:50:00 +0200

    Create appd.tf

---
+++ appd.tf
@@ -0,0 +1,25 @@
+
+ module "appdynamics" {
+  source  = "3191110276/appdynamics/kubernetes"
+  version = "0.2.1"
+
+  appd_account_name        = var.appd_account_name 
+  appd_global_account      = var.appd_global_account 
+  appd_controller_url      = "https://ceer.saas.appdynamics.com:443"
+  appd_controller_hostname = "ceer.saas.appdynamics.com"
+  appd_controller_key      = var.appd_controller_key  
+  appd_username            = var.appd_username  
+  appd_password            = var.appd_password 
+
+  cluster_name = "cnb"
+
+  ns_to_monitor = ["accounting", "appdynamics", "automation", "default", "iks", "iwo", "kube-node-lease", "kube-public", "kube-system", "order", "procurement"]
+  ns_to_instrument = "procurement"
+
+  instrumentation_app_name = "cnb-procurement"
+   
+  deploy_dbcollector = false
+  
+  
+   
+}
